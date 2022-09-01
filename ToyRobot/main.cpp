1fd41ba82a313e629ef8ef71cb15e6eeccb8e2ac
darksharkmark <mark.southcombe18@gmail.com>
2022-09-02 11:06:10 +1200

    initial commit with comments for instructions

---
+++ ToyRobot/main.cpp
@@ -0,0 +1,23 @@
+/*
+* Toy Robot
+* 5 x 5 grid
+* 
+* Commands:
+* PLACE X,Y, [NORTH, EAST, SOUTH, WEST]
+* MOVE
+* LEFT (rotate, not move)
+* RIGHT (rotate, not move)
+* REPORT (print position and direction)
+* 
+* (0,0) -> SOUTH WEST corner
+* Must be placed first
+* any sequence of commands can be issued
+
+	
+*/
+int main()
+{
+
+
+	return 0;
+}
