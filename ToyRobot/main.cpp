fada16b95f8eca33c9794a37a2dc82a60a5e3ddc
darksharkmark <mark.southcombe18@gmail.com>
2022-09-06 13:00:20 +1200

    removed comments

--- ToyRobot/main.cpp
+++ ToyRobot/main.cpp
@@ -1,20 +1,3 @@
-/*
-* Toy Robot
-* 5 x 5 grid
-* 
-* Commands:
-*     - PLACE X,Y, [NORTH, EAST, SOUTH, WEST]
-*     - MOVE
-*     - LEFT (rotate, not move)
-*     - RIGHT (rotate, not move)
-*     - REPORT (print position and direction)
-* 
-* (0,0) -> SOUTH WEST corner
-* Must be placed first
-* otherwise any sequence of commands can be issued
-
-*/
-
 #include "Robot.h"
 #include <vector>
 
